7a09e1ae8577bfdc49fc3a66ec46e0e1bbfe1ae9
Artem <pandawithmelon@gmail.com>
2022-12-27 18:03:07 +0300

    Added MongoDB/Mongoose to hold task data. Minor cosmetic changes.

--- app.js
+++ app.js
@@ -1,23 +1,44 @@
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
 
 const app = express();
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
 
-let listItems = [];
+mongoose.connect('mongodb://127.0.0.1:27017/todolistDB')
+.then(
+    () => console.log('Connected!'),
+    (err) => console.log(err)
+);
+
+const Schema = mongoose.Schema;
+const itemSchema = new Schema({
+    item: {type: String, required: "Field is required!"}
+});
+const Item = mongoose.model("item", itemSchema);
+
 
 app.get("/", (req, res) => {
     const date = new Date();
     const day = date.toLocaleDateString("en-US", {weekday: "long", day: "numeric", month: "long"});
-    
-    res.render("list", {day: day, listItems: listItems});
+    Item.find({}).exec((err, listItems) =>{
+        if(err) {
+            console.log(err)
+        } else {
+            console.log("Data transfered from DB successfully!");
+            res.render("list", {day: day, listItems: listItems});
+        }
+    }); 
 })
 
 app.post("/", (req, res) => {
-    listItems.push(req.body.listItem);
+    const item = req.body.listItem;
+    const newItem = new Item({item: item});
+    newItem.save();
     res.redirect("/");
 })
 
